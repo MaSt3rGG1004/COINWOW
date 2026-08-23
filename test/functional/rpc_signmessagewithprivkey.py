15bc59bd7a6baf647e837539ece9bf3033010d99
COINWOW <gabrielggagnongab@gmail.com>
2026-08-22 21:51:51 -0400

    test: recompute expected signature for COINWOW MESSAGE_MAGIC
    expected_signature was computed under Bitcoin's message magic; recomputed
    deterministically (RFC6979) under COINWOW's own MESSAGE_MAGIC
    ("COINWOW Signed Message:\n"). Address derivation unaffected.

--- test/functional/rpc_signmessagewithprivkey.py
+++ test/functional/rpc_signmessagewithprivkey.py
@@ -30,7 +30,7 @@ class SignMessagesWithPrivTest(COINWOWTestFramework):
 
         self.log.info('test signing with priv_key')
         priv_key = 'cUeKHd5orzT3mz8P9pxyREHfsWtVfgsfDjiZZBcjUBAaGk1BTj7N'
-        expected_signature = 'INbVnW4e6PeRmsv2Qgu8NuopvrVjkcxob+sX8OcZG0SALhWybUjzMLPdAsXI46YZGb0KQTRii+wWIQzRpG/U+S0='
+        expected_signature = 'H/fgjNy0dMnkecdpl7zs3epDYaf0EAji+OpeOX2MeoQjJldKTzCFKuh/7dvcpwKPbXGJ2s+ZYXbPVBS2qN4D2zc='
         signature = self.nodes[0].signmessagewithprivkey(priv_key, message)
         assert_equal(expected_signature, signature)
 
